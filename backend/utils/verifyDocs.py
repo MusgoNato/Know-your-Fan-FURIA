357611c3c9b61212da70aaaa5d3b44c358f63567
Hugo Josué lema das neves <131496781+MusgoNato@users.noreply.github.com>
2025-05-04 20:49:18 -0400

    Update

--- backend/utils/verifyDocs.py
+++ backend/utils/verifyDocs.py
@@ -4,9 +4,15 @@ import re
 from io import BytesIO
 import os
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-os.environ['TESSDATA_PREFIX'] = r'C:\Program Files\Tesseract-OCR\tessdata'
-
+# Detecta se estamos em um ambiente local (Windows) ou em um ambiente de nuvem
+if "TESSDATA_PREFIX" in os.environ:
+    # Se estiver no Streamlit Cloud ou ambiente sem Tesseract instalado
+    pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"  # Caminho padrão do Tesseract no Linux (para Streamlit Cloud)
+else:
+    os.environ['TESSDATA_PREFIX'] = r'C:\Program Files\Tesseract-OCR\tessdata'
+    # Se estiver rodando localmente
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    
 def extract_textFromImg(image_bytes):
     # Converte os bytes da imagem de volta para um objeto Image
     image = Image.open(BytesIO(image_bytes))
